345764281e5b26dc523892775f864296acb3e8dd
zhao <1036978689@qq.com>
2024-12-20 12:01:25 +0800

    token失效优化

--- src/api/request.js
+++ src/api/request.js
@@ -25,6 +25,9 @@ service.interceptors.response.use(
 	response => {
 		const res = response.data
 		if (res.code !== 0) {
+			if (res.code == 401){
+				store.clearUserInfo()
+			}
 			ElMessage.error(res.msg || '请求失败')
 		}
 		return res
